04a1a379955510d50bf1f4dbecc89dda134ba935
Emmanuel Engreso <enrgeso.emmanuel@gmail.com>
2022-08-15 10:06:14 +0800

    add dragable testimonial

--- js/main.js
+++ js/main.js
@@ -30,6 +30,37 @@ const toggleMenu = () => {
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
+const dragTestimonial = () => {
+  const slider = selectElement('.testimonials__wrapper');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+  });
+};
+
+dragTestimonial();
+
 // Buttons Testimonial Control
 
 const leftBtn = selectElement('#btn-testi-left');
@@ -48,6 +79,5 @@ leftBtn.addEventListener('click', () => {
 });
 
 rightBtn.addEventListener('click', () => {
-  console.log('right');
   slider.scrollLeft -= sliderItem.offsetWidth + marginRightNum;
 });
